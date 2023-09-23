a9dcf6bf8b47b2a80736bd2f3722384e51b010d0
ashutosh shelke <111786950+ashutosh-co@users.noreply.github.com>
2023-09-23 12:00:58 +0530

    Update Day 04 Class vs Instance.js

--- Day 04 Class vs Instance.js
+++ Day 04 Class vs Instance.js
@@ -19,17 +19,17 @@ function readLine() {
 }
 
 function Person(initialAge){
-    // Add some more code to run some checks on initialAge
+    
   this.amIOld=function(){
-   // Do some computations in here and print out the correct statement to the console
+   
 
   };
    this.yearPasses=function(){
-          // Increment the age of the person in here
+        
    };
 }
 function Person(initialAge) {
-    // Add some more code to run some checks on initialAge
+    
     if (initialAge < 0) {
         console.log("Age is not valid, setting age to 0.");
         this.age = 0;
@@ -37,7 +37,7 @@ function Person(initialAge) {
         this.age = initialAge;
     }
     this.amIOld = function () {
-        // Do some computations in here and print out the correct statement to the console
+        
         if (this.age < 13) {
             console.log('You are young.');
         } else if (this.age >= 13 && this.age < 18) {
@@ -47,7 +47,7 @@ function Person(initialAge) {
         }
     };
     this.yearPasses = function () {
-        // Increment the age of the person in here
+    
         this.age += 1;
     };
 }
